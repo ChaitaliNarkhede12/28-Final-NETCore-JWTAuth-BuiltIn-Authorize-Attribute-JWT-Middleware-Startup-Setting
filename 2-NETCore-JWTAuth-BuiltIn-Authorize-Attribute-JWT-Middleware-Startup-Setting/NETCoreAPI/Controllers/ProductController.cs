2f52792e670cbb048ed63fa3e6fefafd7f69764f
Chaitali <chaitali_narkhede@epam.com>
2022-07-15 17:53:59 +0530

    Initial-Chek-in

---
+++ 2-NETCore-JWTAuth-BuiltIn-Authorize-Attribute-JWT-Middleware-Startup-Setting/NETCoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetProductList")]
+        public IActionResult GetProductList()
+        {
+            var result = _productService.GetProductList();
+            return Ok(result);
+        }
+    }
+}
